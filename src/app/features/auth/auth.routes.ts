dafed5aed9914ac96c92b436c19ec3a64c6a8308
dadasaheb <dadakashid4141@gmail.com>
2026-04-05 14:08:28 +0530

    Project setup

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+export const AUTH_ROUTES: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Register
+    }
+];
